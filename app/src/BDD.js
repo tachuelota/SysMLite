3664521fe772aba3ec9630887fc68cba5b5e998d
--get <John@RoostertailSoftware.com>
2018-11-25 10:25:01 -0500

    moved to a class

--- app/src/BDD.js
+++ app/src/BDD.js
@@ -5,28 +5,29 @@
  * This should handle most of the work for creating a Block Defination Diagram (BDD).
  */
 
-function buildBDD( ){
+class SysMLiteBDD {
+	constructor() {
+		this.project = app.repository.select( "@Project" )[ 0 ];
+		this.model = app.factory.createModel( { id: "UMLModel", parent: this.project } );
+	};
 
-	var project = app.repository.select( "@Project" )[ 0 ];
-
-	var model = app.factory.createModel( { id: "UMLModel", parent: project } );
-
-	var options = {
-  		id: "SysMLiteBDD",
-  		parent: model,
-  		diagramInitializer: function (dgm) {
-	    dgm.name = "Block Defination Diagram";
-	    dgm.defaultDiagram = true;
+	buildBDD(){
+		var options = {
+			id: "SysMLiteBDD",
+			parent: this.model,
+			diagramInitializer: function( dgm ) {
+				dgm.name = "Block Defination Diagram";
+				dgm.defaultDiagram = true;
+			}
+		}
+		var diag = app.factory.createDiagram( options );
 	}
-}
-
-	var diag = app.factory.createDiagram( options );
-}
-
-
+};
 
 
 function addNewBDD(  ) {
-	buildBDD();
-	// handleShowMessage( "SysMLite BDD" );
-}
+	var a = new SysMLiteBDD();
+	a.buildBDD();
+};
+
+exports.addNewBDD = addNewBDD;
